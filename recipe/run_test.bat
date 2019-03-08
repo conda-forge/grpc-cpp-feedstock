28b98a15b7fce15c5c2ad540d2c711fc2cd9dd38
Wes McKinney <wesm+git@apache.org>
2019-03-08 15:33:40 -0600

    Fix run_test.bat

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -2,7 +2,7 @@
 
 @rem Compile a trivial service definition to C++
 
-protoc -I%RECIPE_DIR% --plugin=protoc-gen-grpc=%PREFIX%/bin/grpc_cpp_plugin.exe --grpc_out=. hello.proto || exit /B
+protoc -I%RECIPE_DIR% --plugin=protoc-gen-grpc=%LIBRARY_PREFIX%/bin/grpc_cpp_plugin.exe --grpc_out=. hello.proto || exit /B
 
 if errorlevel 1 exit 1
 
