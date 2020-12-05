f7be8019e608763b50434f012488845823b0d042
Ray Donnelly <mingw.android@gmail.com>
2020-12-05 22:48:39 +0100

    Use latest abseil-cpp, seems to work today
    Also, change to a static build on all platforms. It turns out we've been
    building this statically on Windows for ever, and in the form of static
    libraries, so I think it best to just go all static here, until we can
    fix Windows.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -5,11 +5,5 @@ set -ex
 # Compile a trivial service definition to C++
 
 protoc -Itests/include --plugin=protoc-gen-grpc=$PREFIX/bin/grpc_cpp_plugin --grpc_out=. hello.proto
-
 test -f hello.grpc.pb.h
 test -f hello.grpc.pb.cc
-
-test -f $PREFIX/lib/libgrpc${SHLIB_EXT}
-test -f $PREFIX/lib/libgrpc_unsecure${SHLIB_EXT}
-test -f $PREFIX/lib/libgrpc++${SHLIB_EXT}
-test -f $PREFIX/lib/libgrpc++_unsecure${SHLIB_EXT}
