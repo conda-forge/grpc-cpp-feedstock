73964490c4092279aa157745915da7dfdd9636a9
Uwe L. Korn <uwe.korn@quantco.com>
2021-08-02 13:16:34 +0200

    Check for presence of plugin binaries

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,11 +4,13 @@ set -ex
 
 # Compile a trivial service definition to C++
 
-protoc -I$RECIPE_DIR --plugin=protoc-gen-grpc=$PREFIX/bin/grpc_cpp_plugin --grpc_out=. hello.proto
+if [[ "${build_platform}" == "${target_platform}" ]]; then
+  protoc -I$RECIPE_DIR --plugin=protoc-gen-grpc=$PREFIX/bin/grpc_cpp_plugin --grpc_out=. hello.proto
+  test -f hello.grpc.pb.h
+  test -f hello.grpc.pb.cc
+fi
 
-test -f hello.grpc.pb.h
-test -f hello.grpc.pb.cc
-
+test -f $PREFIX/bin/grpc_cpp_plugin
 test -f $PREFIX/lib/libgrpc${SHLIB_EXT}
 test -f $PREFIX/lib/libgrpc_unsecure${SHLIB_EXT}
 test -f $PREFIX/lib/libgrpc++${SHLIB_EXT}
