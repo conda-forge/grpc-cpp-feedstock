7a2eee109d96b09598423238f25b96ab709b11c7
H. Vetinari <h.vetinari@gmx.com>
2022-03-06 07:59:01 +1100

    fix indentation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,17 +14,17 @@ if errorlevel 1 exit 1
 cd build-cpp
 
 cmake ..  ^
-      -GNinja ^
-      -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -DgRPC_ABSL_PROVIDER="package" ^
-      -DgRPC_CARES_PROVIDER="package" ^
-      -DgRPC_GFLAGS_PROVIDER="package" ^
-      -DgRPC_PROTOBUF_PROVIDER="package" ^
-      -DgRPC_SSL_PROVIDER="package" ^
-      -DgRPC_RE2_PROVIDER="package" ^
-      -DgRPC_ZLIB_PROVIDER="package"
+    -GNinja ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DgRPC_ABSL_PROVIDER="package" ^
+    -DgRPC_CARES_PROVIDER="package" ^
+    -DgRPC_GFLAGS_PROVIDER="package" ^
+    -DgRPC_PROTOBUF_PROVIDER="package" ^
+    -DgRPC_SSL_PROVIDER="package" ^
+    -DgRPC_RE2_PROVIDER="package" ^
+    -DgRPC_ZLIB_PROVIDER="package"
 
 cmake --build . --config Release --target install
 
