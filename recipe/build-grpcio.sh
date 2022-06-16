e5875948d3f43803e6b63660e4ca93fe81f20335
Marius van Niekerk <marius.v.niekerk@gmail.com>
2022-06-16 19:35:30 -0400

    fixup

--- recipe/build-grpcio.sh
+++ recipe/build-grpcio.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 export GRPC_BUILD_WITH_BORING_SSL_ASM=""
-export GRPC_PYTHON_BUILD_SYSTEM_ZLIB="True"
-export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL="True"
 export GRPC_PYTHON_BUILD_SYSTEM_CARES="True"
-export GRPC_PYTHON_USE_PREBUILT_GRPC_CORE=""
 export GRPC_PYTHON_BUILD_SYSTEM_GRPC_CORE="True"
-export GRPC_PYTHON_BUILD_WITH_CYTHON="True"
+export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL="True"
 export GRPC_PYTHON_BUILD_SYSTEM_RE2="True"
+export GRPC_PYTHON_BUILD_SYSTEM_ZLIB="True"
+export GRPC_PYTHON_BUILD_WITH_CYTHON="True"
+export GRPC_PYTHON_USE_PREBUILT_GRPC_CORE=""
 
 if [[ "${target_platform}" == linux-* ]]; then
     # set these so the default in setup.py are not used
