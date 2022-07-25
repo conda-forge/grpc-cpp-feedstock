4e1adf59557cd973cbeb6bfa588965ff56764b4e
Mark Harfouche <mark.harfouche@gmail.com>
2022-07-25 16:12:56 -0400

    Update build-cpp.bat

--- recipe/build-cpp.bat
+++ recipe/build-cpp.bat
@@ -10,6 +10,7 @@ cd build-cpp
 
 cmake ..  ^
       -GNinja ^
+      -DCMAKE_CXX_STANDARD=14 ^
       -DCMAKE_VERBOSE_MAKEFILE=ON ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
