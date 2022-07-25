74d40311a9a2b2cc36499ac6830f996e47825b3c
Mark Harfouche <mark.harfouche@gmail.com>
2022-07-24 23:04:00 -0400

    Keep CFLAGs

--- recipe/build-cpp.bat
+++ recipe/build-cpp.bat
@@ -1,13 +1,5 @@
 @echo on
 
-@rem The `vs2015_win-64` compiler activate package sets CFLAGS and CXXFLAGS
-@rem to "-MD -GL".  Unfortunately that causes a huge ballooning in static
-@rem library size (more than 100MB per .lib file).  Unsetting those flags
-@rem simply works.
-
-set CFLAGS=
-set CXXFLAGS=
-
 mkdir build-cpp
 if errorlevel 1 exit 1
 
