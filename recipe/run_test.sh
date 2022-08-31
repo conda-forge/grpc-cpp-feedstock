d3ca353dfdb76b291ecc301980d42443b48b2c15
H. Vetinari <h.vetinari@gmx.com>
2022-08-31 19:34:21 +0200

    compile upstream cpp example as test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -7,3 +7,18 @@ if [[ "${build_platform}" == "${target_platform}" ]]; then
   test -f hello.grpc.pb.h
   test -f hello.grpc.pb.cc
 fi
+
+# taken from cd examples/cpp/helloworld
+cd examples/cpp/helloworld
+
+# folder already contains a file called BUILD
+mkdir build-cpp
+cd build-cpp
+
+cmake -G "Ninja" \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_PREFIX_PATH="$PREFIX" \
+    -DCMAKE_MODULE_PATH="$PREFIX/lib/cmake" \
+    ..
+
+cmake --build .
