80701ab5145ca8e5a3e84a6f7b31a2197b8c39a0
H. Vetinari <h.vetinari@gmx.com>
2022-08-31 19:22:57 +0200

    move library tests out of run_test.{sh,bat} and expand

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,15 +1,8 @@
 @echo on
 
 @rem Compile a trivial service definition to C++
-
 protoc -I%RECIPE_DIR% --plugin=protoc-gen-grpc=%LIBRARY_PREFIX%/bin/grpc_cpp_plugin.exe --grpc_out=. hello.proto || exit /B
-
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
 if not exist hello.grpc.pb.h exit 1
 if not exist hello.grpc.pb.cc exit 1
-
-if not exist %PREFIX%\\Library\\lib\\address_sorting.lib exit 1
-if not exist %PREFIX%\\Library\\lib\\gpr.lib exit 1
-if not exist %PREFIX%\\Library\\lib\\grpc.lib exit 1
-if not exist %PREFIX%\\Library\\lib\\grpc++.lib exit 1
