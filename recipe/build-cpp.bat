038308e1cbbf61e7cf6c618a7cbd54d019727854
H. Vetinari <h.vetinari@gmx.com>
2022-09-01 22:40:28 +0200

    switch to shared builds also on windows

--- recipe/build-cpp.bat
+++ recipe/build-cpp.bat
@@ -8,6 +8,7 @@ cd build-cpp
 
 cmake ..  ^
       -GNinja ^
+      -DBUILD_SHARED_LIBS=ON ^
       -DCMAKE_CXX_STANDARD=17 ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
