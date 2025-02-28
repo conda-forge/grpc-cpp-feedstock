66a3cd886b6d5e7904dd8ffc4eb122584b7c4fcb
H. Vetinari <h.vetinari@gmx.com>
2025-02-28 21:54:11 +1100

    reduce compiler noise on linux

--- recipe/build-cpp.sh
+++ recipe/build-cpp.sh
@@ -59,6 +59,9 @@ fi
 if [[ "${target_platform}" == osx-* ]]; then
   # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+else
+  # reduce very noisy compiler warnings
+  CXXFLAGS="${CXXFLAGS} -w"
 fi
 
 mkdir -p build-cpp
